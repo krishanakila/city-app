b29976ef27b5533ebace0dc20c51caf4e5d5676a
Krishan Akila <krishanakila@gmail.com>
2025-07-08 09:45:04 +0530

    main-log img fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
     <div className="mobile-header__wrapper bg-gradient-to-br from-orange-50 to-orange-100">
       <img className="mobile-header__img" src="/mobile-head-w.svg" alt="mobile image header"/>
     <div className="mobile-header__secondary flex justify-between items-center px-4 py-2">
-      <img src="/src/assets/logo.png" className="h-5" />
+      <img src="/logo.png" className="h-5" />
       <div className="flex">
       <div className="mobile-header__right">
         <p className="mobile-header__name">Hi, John Doe</p>
